9faa4fbb280c41edd002aa9e9b3acffb7b1b5c90
gbmumumu <sen.gaobo@gmail.com>
2021-02-16 22:55:06 +0800

    add package problem

--- datastructure/recursion.py
+++ datastructure/recursion.py
@@ -238,11 +238,54 @@ def print_coins(change):
 
 
 coin_used = [0] * 64
-print(make_change([1, 5, 10, 21, 25], 63, [0] * 64))
+print(make_change([1, 5, 10, 25], 63, [0] * 64))
 print_coins(63)
 print(coin_used)
 
 # 背包问题
+tr = [
+    None, {'w': 2, 'v': 3}, {'w': 3, 'v': 4},
+    {'w': 4, 'v': 8}, {'w': 5, 'v': 8}, {'w': 9, 'v': 10},
+]
+max_w = 20
+
+m = {(i, w): 0 for i in range(len(tr)) for w in range(max_w + 1)}
+
+for i in range(1, len(tr)):
+    for w in range(1, max_w + 1):
+        if tr[i]['w'] > w:
+            m[(i, w)] = m[(i - 1, w)]
+        else:
+            m[(i, w)] = max(
+                m[(i - 1, w)],
+                m[(i - 1, w - tr[i]['w'])] + tr[i]['v']
+            )
+
+print(m[len(tr) - 1, max_w])
+
+# 递归
+
+tr = {(2, 3), (3, 4), (4, 8), (5, 8), (9, 10)}
+max_w = 20
+m = {}
+
+
+def thief(tr, w):
+    if not tr or w == 0:
+        m[(tuple(tr), w)] = 0
+        return 0
+    elif (tuple(tr), w) in m:
+        return m[(tuple(tr), w)]
+    else:
+        v_max = 0
+        for t in tr:
+            if t[0] <= w:
+                v = thief(tr - {t}, w - t[0]) + t[1]
+                v_max = max(v_max, v)
+
+        m[(tuple(tr), w)] = v_max
+
+        return v_max
 
 
 if __name__ == "__main__":

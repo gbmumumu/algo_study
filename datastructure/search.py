9b8006ea17759ffcba878b4a5cccdf5d55987490
gbmumumu <sen.gaobo@gmail.com>
2021-02-16 23:10:37 +0800

    add seq search

--- datastructure/search.py
+++ datastructure/search.py
@@ -15,5 +15,21 @@ def sequential_search(lst, item):
     return found
 
 
+def ordered_sequential_search(lst, item):
+    pos = 0
+    found = False
+    stop = False
+    while pos < len(lst) and not found and not stop:
+        if lst[pos] == item:
+            found = True
+        else:
+            if lst[pos] > item:
+                stop = True
+            else:
+                pos += 1
+
+    return found
+
+
 if __name__ == "__main__":
     pass
